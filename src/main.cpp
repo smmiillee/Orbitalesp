e9351ca675d60a5be1e7c8b71525f05c73dcd6ea
smmiillee <smilejpgcsgo@gmail.com>
2026-09-12 06:47:09 -0400

    Implement ESP and Bhop toggles with debug display
    Added ESP and Bhop toggle features with debug information.

--- src/main.cpp
+++ src/main.cpp
@@ -2,36 +2,55 @@
 #include <thread>
 #include <chrono>
 #include <vector>
+#include <cstdio>
 
 #include "memory.h"
 #include "offsets.h"
-#include "math.h"
+#include "cs2math.h"
 #include "sdk.h"
 #include "esp.h"
 #include "bhop.h"
 #include "overlay.h"
+#include "imgui.h"
 
 static std::vector<Player> g_Players;
-static int g_LocalTeam = 0;
+static int  g_LocalTeam  = 0;
+static bool g_EspEnabled  = true;
+static bool g_BhopEnabled = true;
 static OverlayContext g_Overlay;
 
-// Background thread: reads game memory at ~128Hz, feeds g_Players
+// Debug values shown in menu
+static uintptr_t dbg_LocalPawn    = 0;
+static int       dbg_LocalHealth  = 0;
+static int       dbg_PlayerCount  = 0;
+static uintptr_t dbg_ClientBase   = 0;
+
 void MemoryThread() {
     while (g_Overlay.running) {
+        dbg_ClientBase = g_Mem.clientBase;
+
         uintptr_t localPawn = g_Mem.Read<uintptr_t>(
             g_Mem.clientBase + offsets::dwLocalPlayerPawn);
+        dbg_LocalPawn = localPawn;
 
         if (localPawn) {
-            g_LocalTeam = g_Mem.Read<int>(localPawn + cs2::m_iTeamNum);
+            dbg_LocalHealth = g_Mem.Read<int>(localPawn + cs2::m_iHealth);
+            g_LocalTeam     = g_Mem.Read<int>(localPawn + cs2::m_iTeamNum);
 
-            // View matrix
             Matrix4x4 vm = g_Mem.Read<Matrix4x4>(
                 g_Mem.clientBase + offsets::dwViewMatrix);
 
-            g_Players = GetPlayers(vm, g_Overlay.width, g_Overlay.height);
+            if (g_EspEnabled) {
+                g_Players = GetPlayers(vm, g_Overlay.width, g_Overlay.height);
+                dbg_PlayerCount = (int)g_Players.size();
+            } else {
+                g_Players.clear();
+                dbg_PlayerCount = 0;
+            }
         }
 
-        BhopTick();
+        if (g_BhopEnabled)
+            BhopTick();
 
         std::this_thread::sleep_for(std::chrono::milliseconds(8));
     }
@@ -39,27 +58,57 @@ void MemoryThread() {
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
     // Wait for CS2
+    printf("[*] Waiting for CS2...\n");
     while (!FindWindowA("SDL_app", nullptr))
         Sleep(1000);
+    printf("[*] CS2 found.\n");
 
-    // Attach to cs2.exe
+    // Attach memory
+    printf("[*] Attaching to cs2.exe...\n");
     while (!g_Mem.Attach("cs2.exe"))
         Sleep(1000);
+    printf("[*] Attached OK.\n");
 
-    // Get client.dll base
+    // Get client.dll
+    printf("[*] Finding client.dll...\n");
     while (!g_Mem.GetModule("client.dll"))
         Sleep(1000);
+    printf("[*] client.dll base: 0x%llX\n", (unsigned long long)g_Mem.clientBase);
 
     if (!OverlayCreate(g_Overlay))
         return 1;
 
-    // Spin up memory reader
     std::thread memThread(MemoryThread);
     memThread.detach();
 
-    // Render loop
     OverlayRun(g_Overlay, []() {
+        // Always draw ESP
         DrawESP(g_Players, g_LocalTeam);
+
+        // Menu (INSERT to toggle)
+        if (g_Overlay.menuOpen) {
+            ImGui::SetNextWindowSize(ImVec2(280, 220), ImGuiCond_Once);
+            ImGui::SetNextWindowPos(ImVec2(20, 20), ImGuiCond_Once);
+            ImGui::Begin("cs2external", nullptr,
+                ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse);
+
+            ImGui::SeparatorText("Features");
+            ImGui::Checkbox("ESP",  &g_EspEnabled);
+            ImGui::Checkbox("Bhop", &g_BhopEnabled);
+
+            ImGui::Spacing();
+            ImGui::SeparatorText("Debug");
+            ImGui::Text("client.dll : 0x%llX", (unsigned long long)dbg_ClientBase);
+            ImGui::Text("LocalPawn  : 0x%llX", (unsigned long long)dbg_LocalPawn);
+            ImGui::Text("LocalHP    : %d",      dbg_LocalHealth);
+            ImGui::Text("Players    : %d",      dbg_PlayerCount);
+
+            ImGui::Spacing();
+            ImGui::TextDisabled("INSERT = toggle menu");
+            ImGui::TextDisabled("F9 = exit");
+
+            ImGui::End();
+        }
     });
 
     OverlayDestroy(g_Overlay);
