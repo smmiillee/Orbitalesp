9dbdeebd088393b2f6f512ea3653e35ab6cd58ea
smmiillee <smilejpgcsgo@gmail.com>
2026-09-12 23:21:02 -0400

    Update esp.h

--- src/esp.h
+++ src/esp.h
@@ -24,16 +24,16 @@ class ESP {
 public:
     std::vector<PlayerESP> players;
 
-    // Debug counters
     int debug_total_controllers = 0;
     int debug_valid_pawns       = 0;
     int debug_alive             = 0;
     int debug_positioned        = 0;
     int debug_on_screen         = 0;
-    int debug_sample_health     = 0; // raw health read from first team-valid entity
+    int debug_sample_health     = 0;
     int debug_sample_team       = 0;
 
-    void update(const Memory& mem, uintptr_t client_base);
+    // screen_w/h passed in so projection matches actual resolution
+    void update(const Memory& mem, uintptr_t client_base, int screen_w, int screen_h);
 
     static bool world_to_screen(const Vec3& world, Vec2& screen,
                                  const Matrix4x4& vm, int screen_w, int screen_h);
