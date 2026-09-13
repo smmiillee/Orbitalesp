780681fe35f98960b0a042f9044a526007cdb481
smmiillee <smilejpgcsgo@gmail.com>
2026-09-12 23:29:13 -0400

    Update esp.h

--- src/esp.h
+++ src/esp.h
@@ -1,6 +1,7 @@
 // --- src/esp.h ---
 #pragma once
 #include <vector>
+#include <mutex>
 #include "memory.h"
 #include "offsets.h"
 
@@ -23,16 +24,8 @@ struct PlayerESP {
 class ESP {
 public:
     std::vector<PlayerESP> players;
+    std::mutex             players_mutex;
 
-    int debug_total_controllers = 0;
-    int debug_valid_pawns       = 0;
-    int debug_alive             = 0;
-    int debug_positioned        = 0;
-    int debug_on_screen         = 0;
-    int debug_sample_health     = 0;
-    int debug_sample_team       = 0;
-
-    // screen_w/h passed in so projection matches actual resolution
     void update(const Memory& mem, uintptr_t client_base, int screen_w, int screen_h);
 
     static bool world_to_screen(const Vec3& world, Vec2& screen,
